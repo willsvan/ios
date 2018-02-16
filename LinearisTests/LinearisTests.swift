ad36d2b17aba09848309eb056dbefa7ab071d15a
Thunpisit Amnuaikiatloet <fluke_family@hotmail.com>
2018-02-16 14:17:37 -0600

    Initial Commit

---
+++ LinearisTests/LinearisTests.swift
@@ -0,0 +1,36 @@
+//
+//  LinearisTests.swift
+//  LinearisTests
+//
+//  Created by Thunpisit Amnuaikiatloet on 2/16/18.
+//  Copyright © 2018 Thunpisit Amnuaikiatloet. All rights reserved.
+//
+
+import XCTest
+@testable import Linearis
+
+class LinearisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
